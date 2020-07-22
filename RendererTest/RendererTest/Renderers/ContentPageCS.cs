0e6c050bbecaeee3cfbdf0a2de9b0cacd2540672
roamschemer <roamchemer@gmail.com>
2020-07-22 10:59:53 +0900

    キーボードの値を表示

--- RendererTest/RendererTest/Renderers/ContentPageCS.cs
+++ RendererTest/RendererTest/Renderers/ContentPageCS.cs
@@ -2,15 +2,25 @@
 
 namespace RendererTest.Renderers {
     public class ContentPageCS : ContentPage {
+
+        public static readonly BindableProperty DownKeyProperty =
+            BindableProperty.Create(nameof(DownKey), //名前
+                                    typeof(string),　//型
+                                    typeof(ContentPageCS), //自クラス
+                                    string.Empty,    //初期値
+                                    propertyChanged: (bindable, oldValue, newValue) => //変更があったことを感知するイベントハンドラ
+                                    {
+                                        ((ContentPageCS)bindable).DownKey = newValue;
+                                    },
+                                    defaultBindingMode: BindingMode.TwoWay); //初期バインディング方向
+        public object DownKey {
+            get => GetValue(DownKeyProperty);
+            set => SetValue(DownKeyProperty, value);
+        }
+
         public void OnKeyDown(string key) {
-            switch (key) {
-                case "A":
-                    // Aを入力した時の処理
-                    break;
-                case "B":
-                    // Bを入力した時の処理
-                    break;
-            }
+            DownKey = key;
+            DownKey = string.Empty;
         }
     }
 }
--- RendererTest/RendererTest/ViewModels/PageRendererTestPageViewModel.cs
+++ RendererTest/RendererTest/ViewModels/PageRendererTestPageViewModel.cs
@@ -1,13 +1,23 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using Reactive.Bindings;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Xamarin.Forms;
 
 namespace RendererTest.ViewModels {
     public class PageRendererTestPageViewModel : BindableBase {
-        public PageRendererTestPageViewModel() {
 
+        public ReactiveProperty<string> DownKey { get; } = new ReactiveProperty<string>();
+        public ReactiveProperty<string> DownKeyLabel { get; } = new ReactiveProperty<string>();
+
+        public PageRendererTestPageViewModel() {
+            DownKey.Subscribe(x => {
+                if (!string.IsNullOrEmpty(x)) {
+                    DownKeyLabel.Value = x;
+                }
+            });
         }
     }
 }
