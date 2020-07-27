1e65fdc2affa2badb0f79385f6b089aa89424bfe
roamschemer <roamchemer@gmail.com>
2020-07-27 11:02:50 +0900

    イベントに変更

--- RendererTest/RendererTest/Renderers/ContentPageCS.cs
+++ RendererTest/RendererTest/Renderers/ContentPageCS.cs
@@ -1,26 +1,20 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 
 namespace RendererTest.Renderers {
+
+    public class OnKeyDownEventArgs : EventArgs {
+        /// <summary>押されたキー</summary>
+        public string key;
+    }
+
     public class ContentPageCS : ContentPage {
 
-        public static readonly BindableProperty DownKeyProperty =
-            BindableProperty.Create(nameof(DownKey), //名前
-                                    typeof(string),　//型
-                                    typeof(ContentPageCS), //自クラス
-                                    string.Empty,    //初期値
-                                    propertyChanged: (bindable, oldValue, newValue) => //変更があったことを感知するイベントハンドラ
-                                    {
-                                        ((ContentPageCS)bindable).DownKey = newValue;
-                                    },
-                                    defaultBindingMode: BindingMode.TwoWay); //初期バインディング方向
-        public object DownKey {
-            get => GetValue(DownKeyProperty);
-            set => SetValue(DownKeyProperty, value);
-        }
+        public delegate void OnKeyDownEventHandler(object sender, OnKeyDownEventArgs e);
+        public event OnKeyDownEventHandler KeyDown;
 
-        public void OnKeyDown(string key) {
-            DownKey = key;
-            DownKey = string.Empty;
+        public void OnKeyDown(OnKeyDownEventArgs e) {
+            KeyDown?.Invoke(this, e);
         }
     }
 }
--- RendererTest/RendererTest/ViewModels/PageRendererTestPageViewModel.cs
+++ RendererTest/RendererTest/ViewModels/PageRendererTestPageViewModel.cs
@@ -9,13 +9,13 @@ using Xamarin.Forms;
 namespace RendererTest.ViewModels {
     public class PageRendererTestPageViewModel : BindableBase {
 
-        public ReactiveProperty<string> DownKey { get; } = new ReactiveProperty<string>();
+        public ReactiveProperty<string> DownKey { get; } = new ReactiveProperty<string>(mode: ReactivePropertyMode.RaiseLatestValueOnSubscribe);
         public ReactiveProperty<string> DownKeyLabel { get; } = new ReactiveProperty<string>();
 
         public PageRendererTestPageViewModel() {
             DownKey.Subscribe(x => {
                 if (!string.IsNullOrEmpty(x)) {
-                    DownKeyLabel.Value = x;
+                    DownKeyLabel.Value = DownKeyLabel.Value + x;
                 }
             });
         }
