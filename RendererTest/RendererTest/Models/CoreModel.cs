2313d55dd145876a06fe5b5030a78b4af17eaf8d
roamschemer <roamchemer@gmail.com>
2020-07-28 23:59:22 +0900

    Model層を作成

---
+++ RendererTest/RendererTest/Models/CoreModel.cs
@@ -0,0 +1,15 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RendererTest.Models {
+    public class CoreModel : BindableBase {
+        public string KeyHistory { get => keyHistory; set => SetProperty(ref keyHistory, value); }
+        private string keyHistory;
+
+        public void SetKeyHistory(string key) {
+            KeyHistory += key;
+        }
+    }
+}
--- RendererTest/RendererTest/ViewModels/PageRendererTestPageViewModel.cs
+++ RendererTest/RendererTest/ViewModels/PageRendererTestPageViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Navigation;
 using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using RendererTest.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,14 +11,14 @@ using System.Reactive.Linq;
 using Xamarin.Forms;
 
 namespace RendererTest.ViewModels {
-    public class PageRendererTestPageViewModel : BindableBase {
+    public class PageRendererTestPageViewModel : ViewModelBase {
 
         public ReactiveCommand<string> KeyDownCommand { get; }
-        public ReactiveProperty<string> Key { get; } = new ReactiveProperty<string>();
+        public ReadOnlyReactiveProperty<string> KeyHistory { get; }
 
-        public PageRendererTestPageViewModel() {
-            KeyDownCommand = new ReactiveCommand<string>().WithSubscribe(x => Key.Value = Key.Value + x);
-
+        public PageRendererTestPageViewModel(INavigationService navigationService, CoreModel coreModel) : base(navigationService) {
+            KeyHistory = coreModel.ObserveProperty(x => x.KeyHistory).ToReadOnlyReactiveProperty();
+            KeyDownCommand = new ReactiveCommand<string>().WithSubscribe(x => coreModel.SetKeyHistory(x));
         }
     }
 }
