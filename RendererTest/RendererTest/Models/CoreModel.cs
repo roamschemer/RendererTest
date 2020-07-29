ebf6ad993289947ccbe3f69077783071a013ac41
roamschemer <roamchemer@gmail.com>
2020-07-29 09:43:55 +0900

    ショートカットキーの動作追加

--- RendererTest/RendererTest/Models/CoreModel.cs
+++ RendererTest/RendererTest/Models/CoreModel.cs
@@ -5,8 +5,19 @@ namespace RendererTest.Models {
         public string KeyHistory { get => keyHistory; set => SetProperty(ref keyHistory, value); }
         private string keyHistory;
 
+        public string MethodHistory { get => methodHistory; set => SetProperty(ref methodHistory, value); }
+        private string methodHistory;
+
+
         public void SetKeyHistory(string key) {
             KeyHistory += key;
+            if (key == "A") MethodA();
+            if (key == "B") MethodB();
+            if (key == "C") MethodC();
         }
+
+        public void MethodA() => MethodHistory += "Method A を実行しました。\n";
+        public void MethodB() => MethodHistory += "Method B を実行しました。\n";
+        public void MethodC() => MethodHistory += "Method C を実行しました。\n";
     }
 }
--- RendererTest/RendererTest/ViewModels/PageRendererTestPageViewModel.cs
+++ RendererTest/RendererTest/ViewModels/PageRendererTestPageViewModel.cs
@@ -2,16 +2,26 @@
 using Reactive.Bindings;
 using Reactive.Bindings.Extensions;
 using RendererTest.Models;
+using System;
 
 namespace RendererTest.ViewModels {
     public class PageRendererTestPageViewModel : ViewModelBase {
 
         public ReactiveCommand<string> KeyDownCommand { get; }
         public ReadOnlyReactiveProperty<string> KeyHistory { get; }
+        public ReactiveCommand CommandA { get; } = new ReactiveCommand();
+        public ReactiveCommand CommandB { get; } = new ReactiveCommand();
+        public ReactiveCommand CommandC { get; } = new ReactiveCommand();
+        public ReadOnlyReactiveProperty<string> MethodHistory { get;}
 
         public PageRendererTestPageViewModel(INavigationService navigationService, CoreModel coreModel) : base(navigationService) {
             KeyHistory = coreModel.ObserveProperty(x => x.KeyHistory).ToReadOnlyReactiveProperty();
             KeyDownCommand = new ReactiveCommand<string>().WithSubscribe(x => coreModel.SetKeyHistory(x));
+
+            MethodHistory = coreModel.ObserveProperty(x => x.MethodHistory).ToReadOnlyReactiveProperty();
+            CommandA.Subscribe(_ => coreModel.MethodA());
+            CommandB.Subscribe(_ => coreModel.MethodB());
+            CommandC.Subscribe(_ => coreModel.MethodC());
         }
     }
 }
